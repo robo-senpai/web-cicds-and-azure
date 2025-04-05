805cbc88282b88c6bc3498df27f2fb860941c971
daemoon <mintchocolatemoondae@gmail.com>
2025-04-05 14:13:44 +0200

    Add test project and initial test cases for MeritoWebApp

---
+++ MeritoWebApp.Tests/WebAppTests.cs
@@ -0,0 +1,21 @@
+﻿using Xunit;
+
+namespace MeritoWebApp.Tests;
+
+public class WebAppTests
+{
+    [Fact]
+    public void Test1()
+    {
+        Assert.True(true);
+    }
+}
+
+public class HomeControllerTests
+{
+    [Fact]
+    public void Test_HelloWorld()
+    {
+        Assert.Equal("Hello World!", "Hello World!");
+    }
+}
